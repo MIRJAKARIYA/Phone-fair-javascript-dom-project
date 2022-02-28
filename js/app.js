47c887d11e086e48fe6d10316183daea5d2df375
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-02-28 22:27:38 +0600

    card making is done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ const searchPhone = () => {
     const searchText = document.getElementById('search-field');
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText.value}`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => putResult(data.data))
     searchText.value = '';
+}
+
+// putting search results into result container
+const putResult = phones => {
+    const resultContainer = document.getElementById('result-container');
+    for(const phone of phones){
+        
+    }
 }
